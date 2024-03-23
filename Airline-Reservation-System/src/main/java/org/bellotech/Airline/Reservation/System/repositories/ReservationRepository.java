13bf811dfe9d3a9055faf0a46edcb09c454abd42
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-23 20:52:05 +0000

    reserv repo refac commits

--- Airline-Reservation-System/src/main/java/org/bellotech/Airline/Reservation/System/repositories/ReservationRepository.java
+++ Airline-Reservation-System/src/main/java/org/bellotech/Airline/Reservation/System/repositories/ReservationRepository.java
@@ -5,7 +5,7 @@ import org.bellotech.Airline.Reservation.System.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
 }
